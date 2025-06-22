fe61fb3fdc9dbf4c9258c307d89b6cff381623b6
Yukari55 <Yukari.Tech.Design@gmail.com>
2025-06-22 16:51:17 +0200

    WIP: completed next page

---
+++ ja/js/main.js
@@ -0,0 +1,8 @@
+const images = document.querySelectorAll(".bg-img");
+let current =0;
+
+setInterval(() => {
+    images[current].classList.remove('active');
+    current = (current + 1)% images.length;
+    images[current].classList.add('active');
+}, 9000);
