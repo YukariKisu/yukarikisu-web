c30a276bb0df13efd3c44fc9b288ab34e1873ddd
Yukari55 <Yukari.Tech.Design@gmail.com>
2025-06-23 22:40:51 +0200

    WIP: Added en/About Me page

--- common/js/footer.js
+++ common/js/footer.js
@@ -1,6 +1,6 @@
 document.getElementById("footer").innerHTML = `
   <footer>
-    <p style="text-align: center; padding: 20px; font-size: 0.9em;">
+    <p style="text-align: center; padding: 20px; font-size: 0.9em; mergin-bottom: 2rem;">
       &copy; 2025 Yukari Kisu. All rights reserved.
     </p>
   </footer>
