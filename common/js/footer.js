294d81dc89afe209c2e02f39b18b1d7eeec419e7
Yukari55 <Yukari.Tech.Design@gmail.com>
2025-06-24 11:37:25 +0200

    WIP: Added ja/aboutme page

--- common/js/footer.js
+++ common/js/footer.js
@@ -1,7 +1,12 @@
 document.getElementById("footer").innerHTML = `
   <footer>
+  <div class="social-icons-mobile">
+    <a href="https://www.instagram.com/" target="_blank"><img src="img/instagram.svg" alt="Instagram"></a>
+    <a href="https://www.linkedin.com/" target="_blank"><img src="img/linkedin.svg" alt="LinkedIn"></a>
+    <a href="https://github.com/" target="_blank"><img src="img/github.svg" alt="GitHub"></a>
+  </div>
     <p style="text-align: center; padding: 20px; font-size: 0.9em; mergin-bottom: 2rem;">
-      &copy; 2025 Yukari Kisu. All rights reserved.
+      &copy; 2025 Yukari Kisu. All rights reserved. Designed & coded by Yukari.
     </p>
   </footer>
 `;
