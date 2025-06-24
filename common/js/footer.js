e4c2ccf8c04d2233d0262545d811506ed8830bf4
Yukari55 <Yukari.Tech.Design@gmail.com>
2025-06-24 22:21:27 +0200

    WIP: Added en/concept

--- common/js/footer.js
+++ common/js/footer.js
@@ -1,12 +1,29 @@
-document.getElementById("footer").innerHTML = `
-  <footer>
-  <div class="social-icons-mobile">
-    <a href="https://www.instagram.com/" target="_blank"><img src="img/instagram.svg" alt="Instagram"></a>
-    <a href="https://www.linkedin.com/" target="_blank"><img src="img/linkedin.svg" alt="LinkedIn"></a>
-    <a href="https://github.com/" target="_blank"><img src="img/github.svg" alt="GitHub"></a>
-  </div>
-    <p style="text-align: center; padding: 20px; font-size: 0.9em; mergin-bottom: 2rem;">
+/*document.getElementById("footer").innerHTML = `
+  <footer class="credit">
+    <div class="social-icons-mobile">
+      <a href="https://www.instagram.com/" target="_blank"><img src="img/instagram.svg" alt="Instagram"></a>
+      <a href="https://www.linkedin.com/" target="_blank"><img src="img/linkedin.svg" alt="LinkedIn"></a>
+      <a href="https://github.com/" target="_blank"><img src="img/github.svg" alt="GitHub"></a>
+    </div>
+    <p>
       &copy; 2025 Yukari Kisu. All rights reserved. Designed & coded by Yukari.
     </p>
   </footer>
+`;*/
+
+
+document.getElementById("footer").innerHTML = `
+  <div class="credit">
+    <div class="social-icons-mobile">
+      <a href="https://www.instagram.com/" target="_blank"><img src="img/instagram.svg" alt="Instagram"></a>
+      <a href="https://www.linkedin.com/" target="_blank"><img src="img/linkedin.svg" alt="LinkedIn"></a>
+      <a href="https://github.com/" target="_blank"><img src="img/github.svg" alt="GitHub"></a>
+    </div>
+    <p>
+      &copy; 2025 Yukari Kisu. All rights reserved. Designed & coded by Yukari.
+    </p>
+  </div>
 `;
+
+
+
