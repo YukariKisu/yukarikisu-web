8a5e5d7fb2bbce1b07ea0ee71430e31edc29508e
Yukari55 <Yukari.Tech.Design@gmail.com>
2025-06-25 18:44:20 +0200

    WIP: Added en/work1

--- common/js/header.js
+++ common/js/header.js
@@ -1,30 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("header").innerHTML = `
+  const lang = localStorage.getItem("lang") || "en";
+  const base = lang === "ja" ? "/ja" : "/en";
+
+  const navHTML = `
     <header>
-      <!-- Mobile Bamburger menu -->
       <div class="hamburger-menu" id="hamburgerMenu">☰</div>
 
-      <!-- PC Navi (images) -->
       <nav>
         <ul class="nav-buttons">
-          <li><a href="../index.html"><img src="../common/img/nav-toppage.png" alt="Top Page"></a></li>
-          <li><a href="aboutme.html"><img src="../common/img/nav-about.png" alt="About"></a></li>
-          <li><a href="concept.html"><img src="../common/img/nav-concept.png" alt="Concept"></a></li>
-          <li><a href="projects.html"><img src="../common/img/nav-projects.png" alt="Projects"></a></li>
-          <li><a href="../technical-docs/index.html"><img src="../common/img/nav-techdocs.png" alt="Docs"></a></li>
+          <li><a href="/index.html"><img src="/common/img/nav-toppage.png" alt="Top Page"></a></li>
+          <li><a href="${base}/aboutme.html"><img src="/common/img/nav-about.png" alt="About"></a></li>
+          <li><a href="${base}/concept.html"><img src="/common/img/nav-concept.png" alt="Concept"></a></li>
+          <li><a href="${base}/works.html"><img src="/common/img/nav-projects.png" alt="Projects"></a></li>
+          <li><a href="/technical-docs/index.html"><img src="/common/img/nav-techdocs.png" alt="Docs"></a></li>
         </ul>
       </nav>
 
-      <!-- Mobile Navi (images) -->
       <div class="mobile-nav" id="mobileNav">
-        <a href="../index.html"><img src="../common/img/nav-toppage.png" alt="Top"></a>
-        <a href="aboutme.html"><img src="../common/img/nav-about.png" alt="About"></a>
-        <a href="concept.html"><img src="../common/img/nav-concept.png" alt="Concept"></a>
-        <a href="projects.html"><img src="../common/img/nav-projects.png" alt="Projects"></a>
-        <a href="../technical-docs/index.html"><img src="../common/img/nav-techdocs.png" alt="Docs"></a>
+        <a href="/index.html"><img src="/common/img/nav-toppage.png" alt="Top"></a>
+        <a href="${base}/aboutme.html"><img src="/common/img/nav-about.png" alt="About"></a>
+        <a href="${base}/concept.html"><img src="/common/img/nav-concept.png" alt="Concept"></a>
+        <a href="${base}/works.html"><img src="/common/img/nav-projects.png" alt="Projects"></a>
+        <a href="/technical-docs/index.html"><img src="/common/img/nav-techdocs.png" alt="Docs"></a>
       </div>
     </header>
   `;
+
+  document.getElementById("header").innerHTML = navHTML;
 
   const hamburger = document.getElementById("hamburgerMenu");
   const mobileNav = document.getElementById("mobileNav");
