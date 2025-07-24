c415440f986cdd03315c7814128f43b13e612ff2
Yukari55 <Yukari.Tech.Design@gmail.com>
2025-07-24 17:09:39 +0200

    fixed footer icon

--- common/js/footer.js
+++ common/js/footer.js
@@ -1,9 +1,9 @@
 document.getElementById("footer").innerHTML = `
   <div class="credit">
     <div class="social-icons-mobile">
-      <a href="https://www.instagram.com/" target="_blank"><img src="/common/img/instagram.svg" alt="Instagram" class="social-icon"></a>
-      <a href="https://www.linkedin.com/" target="_blank"><img src="/common/img/linkedin.png" alt="LinkedIn" class="social-icon"></a>
-      <a href="https://github.com/" target="_blank"><img src="/common/img/github.svg" alt="GitHub" class="social-icon"></a>
+      <a href="https://www.instagram.com/yukari.kisu" target="_blank"><img src="/common/img/instagram.svg" alt="Instagram" class="social-icon"></a>
+      <a href="https://www.linkedin.com/in/yukari-kisu-8aa5b3339" target="_blank"><img src="/common/img/linkedin.png" alt="LinkedIn" class="social-icon"></a>
+      <a href="https://github.com/YukariKisu" target="_blank"><img src="/common/img/github.svg" alt="GitHub" class="social-icon"></a>
     </div>
     <p>&copy; 2025 Yukari Kisu. All rights reserved. Designed & coded by Yukari.</p>
   </div>
